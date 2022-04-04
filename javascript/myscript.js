24dcd7b6c4280efcf47289308c629c1c27f601e4
Siyu Zhang <89881842+VenusGoyard@users.noreply.github.com>
2022-04-03 23:00:07 -0400

    Add files via upload

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,7 +1,19 @@
 console.log("hellow world");
 
 console.log(  document.getElementById("field1").value  );
-document.getElementById("myButton").onclick = function() {
+document.getElementById("button1").onclick = function() {
         document.getElementById("word01").innerHTML = document.getElementById("field1").value;
     document.getElementById("story").style.display = "block";
     }
+
+console.log(  document.getElementById("field2").value  );
+document.getElementById("button2").onclick = function() {
+        document.getElementById("word02").innerHTML = document.getElementById("field2").value;
+    document.getElementById("story").style.display = "block";
+        }
+
+console.log(  document.getElementById("field3").value  );
+document.getElementById("button3").onclick = function() {
+        document.getElementById("word03").innerHTML = document.getElementById("field3").value;
+    document.getElementById("story").style.display = "block";
+                }
