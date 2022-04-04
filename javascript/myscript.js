39b5e30a33262b7d6469148654172c86fc2638fe
Siyu Zhang <89881842+VenusGoyard@users.noreply.github.com>
2022-04-04 19:10:46 -0400

    Add files via upload

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -4,7 +4,16 @@ console.log(  document.getElementById("field1").value  );
 document.getElementById("button1").onclick = function() {
         document.getElementById("word01").innerHTML = document.getElementById("field1").value;
     document.getElementById("story").style.display = "block";
-    }
+    document.getElementById("word04").innerHTML = document.getElementById("field1").value;
+    document.getElementById("story").style.display = "block";
+    document.getElementById("word05").innerHTML = document.getElementById("field1").value;
+    document.getElementById("story").style.display = "block";
+    document.getElementById("word06").innerHTML = document.getElementById("field1").value;
+    document.getElementById("story").style.display = "block";
+    document.getElementById("word07").innerHTML = document.getElementById("field1").value;
+    document.getElementById("story").style.display = "block";
+    }document.getElementById("word08").innerHTML = document.getElementById("field1").value;
+    document.getElementById("story").style.display = "block";
 
 console.log(  document.getElementById("field2").value  );
 document.getElementById("button2").onclick = function() {
